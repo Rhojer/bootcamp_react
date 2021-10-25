a388aca6627e46fd7c4d650fea252bb522c6d891
DEScontrol <34425397+Rhojer@users.noreply.github.com>
2021-10-24 23:26:16 -0300

    ejercicios en parte java

--- ejercicios/react1/src/index.js
+++ ejercicios/react1/src/index.js
@@ -2,28 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 const Header = ({course}) => <h1>{course}</h1>
 
-const Content = ({part, exercises}) =>{
-  return <p>{part} {exercises}</p>
+const Content = ({part}) =>{
+ 
+   return <p>{part.name} {part.exercises}</p>
 }
+
 const Total = ({total}) =>{
   return <p>Number of exercises {total}</p>
 }
 const App = () => {
-  const course = 'Half Stack application development.'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const total = part1.exercises+ part2.exercises + part3.exercises
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total total={total}/>
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+       <Total total={total} />
   
     </div>
   )
