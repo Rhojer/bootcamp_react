47344e05d1e45553e9f7bfa613e3a6b195ad7f28
DEScontrol <34425397+Rhojer@users.noreply.github.com>
2022-01-23 16:20:45 -0300

    ejercicio 3-Listas

--- ejercicios/react1/src/App.js
+++ ejercicios/react1/src/App.js
@@ -2,64 +2,94 @@ import {useState} from 'react'
 import './App.css';
 
 function App() { const [counter, setCounter] = useState(0);
-  const ListOfClick = ({click}) => {
-    return <p>{click}</p>;
-  };
-  
-  
-  const WarningNotClicks = ()=><p>no hay ningun click</p>
-  const [counters, setCounters] = useState({
-    right: 0,
-    left: 0,
-    menssage: "mensaje de estado"
-  });
-
-  const [click, setClick] = useState([]);
-
-  const handleClickLeft = () => {
-    setCounters({
-      ...counters,
-      left: counters.left + 1
-    });
-    setClick((prevClick) => [...prevClick, "L"]);
-  };
-  const handleClickRight = () => {
-    setCounters({
-      ...counters,
-      right: counters.right + 1
-    });
-    setClick((prevClick) => [...prevClick, "R"]);
-  };
-
-  const handleClickReset = () => {
-    setCounters({
-      ...counters,
-      left: 0,
-      right: 0
-    });
-    setClick([]);
-  };
-
-  
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <p></p>
-      {counters.left}
-      <button onClick={handleClickLeft}>izq</button>
-      <button onClick={handleClickReset}>reset</button>
-      <button onClick={handleClickRight}>der</button>
-      {counters.right}
-      <p>{counters.menssage}</p>
-      <p> total de clicks: {click.length}</p>
-      {click.length === 0 
-      ?
-      <WarningNotClicks/>
-      :
-      <ListOfClick click={click} />
-      }
+      <ListarCursos course={courses} />
+      {/* {courses.map((course)=>{
+        return (
+          <div key={course.id}>
+        <h1>{course.name}</h1>
+        {course.parts.map((part)=>{
+          return (<div key={part.id}>
+          <p>{part.name}: {part.exercises} </p>
+          </div>
+          )
+        })}
+       </div>
+        
+        )
+      })} */}
     </div>
   );
+};
+const ListarCursos = ({ course }) => {
+  
+  return course.map((course) => {
+    let total = 0;
+    return (
+      <div key={course.id}>
+        <h1>{course.name}</h1>
+        {course.parts.map((part) => {
+          total = total + part.exercises;
+          return (
+            <div key={part.id}>
+              <p>
+                {part.name}: {part.exercises}
+              </p>
+            </div>
+          );
+        })}
+        <p>
+          <strong>total of exercises: {total}</strong>
+        </p>
+      </div>
+    );
+  });
 }
 
 export default App;
