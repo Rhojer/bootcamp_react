5b04ef0d5156f38c8665ec2b0f580dc08cf70944
DEScontrol <34425397+Rhojer@users.noreply.github.com>
2022-01-23 16:41:43 -0300

    ejercicio 3-2 agregar y buscar en lista(phonebook)

--- ejercicios/react1/src/App.js
+++ ejercicios/react1/src/App.js
@@ -1,95 +1,60 @@
 import {useState} from 'react'
 import './App.css';
 
-function App() { const [counter, setCounter] = useState(0);
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4
-        }
-      ]
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ];
-
-  return (
-    <div>
-      <ListarCursos course={courses} />
-      {/* {courses.map((course)=>{
-        return (
-          <div key={course.id}>
-        <h1>{course.name}</h1>
-        {course.parts.map((part)=>{
-          return (<div key={part.id}>
-          <p>{part.name}: {part.exercises} </p>
-          </div>
-          )
-        })}
-       </div>
+function App() { 
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number:'+584149693852' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filterN, setfilterN ] = useState('')
+  const handleChangeName = (event) =>{
+    setNewName(event.target.value)
+    
+  }
+  const handleChangeNumber = (event) =>{
+    setNewNumber(event.target.value)
+  }
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    setPersons([...persons,{name:newName, number:newNumber}])
+    console.log(persons)
+    
         
-        )
-      })} */}
+  }
+
+  const filterNumbers = (event) => {
+    setfilterN(event.target.value)
+
+  }
+  return (
+    
+    <div>
+      <h2>Phonebook</h2>
+      <p><strong>FILTRAR</strong></p>
+      <input onChange={filterNumbers} />
+      <form onSubmit={handleSubmit}>
+      <h2>Add a New</h2>
+        <div>
+          <p>name: <input onChange={handleChangeName} /></p>
+          number: <input onChange={handleChangeNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+     {persons
+     .filter(person=>{
+       return person.name.toLowerCase().indexOf(filterN.toLowerCase()) > -1
+     })
+     .map(person=>{
+      
+       return <p>{person.name} {person.number}</p>
+     })}
     </div>
-  );
-};
-const ListarCursos = ({ course }) => {
-  
-  return course.map((course) => {
-    let total = 0;
-    return (
-      <div key={course.id}>
-        <h1>{course.name}</h1>
-        {course.parts.map((part) => {
-          total = total + part.exercises;
-          return (
-            <div key={part.id}>
-              <p>
-                {part.name}: {part.exercises}
-              </p>
-            </div>
-          );
-        })}
-        <p>
-          <strong>total of exercises: {total}</strong>
-        </p>
-      </div>
-    );
-  });
+  )
 }
+
 
 export default App;
